26743a7aa9376201faa5b0237b5ca290ebdbc343
Abdihakim Hassan <h_abdihakimi@yahoo.com>
2020-04-01 11:29:20 +0300

    Adding event to js file

---
+++ js/main.js
@@ -0,0 +1,32 @@
+//Declaring variavle for the forms
+var button = document.getElementById('myButton');
+var delButton= document.getElementById('DelButton');
+
+// Adding event lister to the form
+button.addEventListener('click', showResult);
+// Adding Delete 
+delButton.addEventListener('click', deleteResult);
+
+
+// Declaring my function 
+function showResult(e){
+    e.preventDefault();
+
+    console.log('Who are you');
+
+};
+
+
+
+
+
+// Deleting Items function
+
+function deleteResult(e){
+    e.preventDefault();
+
+};
+
+
+
+
