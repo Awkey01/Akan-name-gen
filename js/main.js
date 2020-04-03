a43249fb2ad119458954e1a7f8db3be6910036ea
Abdihakim Hassan <h_abdihakimi@yahoo.com>
2020-04-03 09:49:58 +0300

    modified the js file

--- js/main.js
+++ js/main.js
@@ -1,57 +1,39 @@
-//Declaring variable for the Buttons
-var button = document.getElementById('myButton');
-var delButton= document.getElementById('DelButton');
-
-// Adding event lister to the form
-button.addEventListener('click', showResult);
-// Adding Delete 
-delButton.addEventListener('click', deleteResult);
-
-
-// Declaring my function 
-function showResult(e){
-    e.preventDefault();
-    var dd = document.getElementById("dd").value; 
-	var mm = document.getElementById("mm").value;
-	var yy = document.getElementById("yy").value; 
-	var male = document.getElementById("male").checked;
-    var female = document.getElementById("female").checked; 
-    var  months = ['January','February','March','April','May','June',  'July','August','September','October','November','December'];
+function ShowResult() {
+	var dd = document.getElementById("dd").value;
+	var mm = document.getElementById("mm").value;  
+	var yy = document.getElementById("yy").value;
+	var male = document.getElementById("male").checked; 
+	var female = document.getElementById("female").checked; 
 	var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; 
-	var male_names = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]; 
+	var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]; 
 	var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]; 
-	var birth_Date = new Date(mm + '/' + dd + '/' + yy);
-    var dayOfTheWeek = birth_Date.getDay(); 
-    var month_of_the_year = birth_Date.getMonth();
+	var birthDate = new Date(dd + '/' + mm + '/' + yy);
+	var dayOfTheWeek = birthDate.getDay(); //output day of the week
 
+	if (dd <= 0 || dd > 31) { 
+		document.getElementById("error").innerText = "Oops didn't selected a valid date!"  
+	} else if ((mm === "September" || mm === "April" || mm === "June" || mm === "November") && (dd <= 0 || dd > 30)) {
+		document.getElementById("error").innerText = "Oops didn't selected a valid date!"  
+	} else if (mm === "------") { 
+		document.getElementById("error").innerText = "Oops you didn't selected a month!"
+		
+	} else if (yy <= 0 && yy <2020) { 
+		document.getElementById("error").innerText = "Please enter a valid year!" 
 
+	} else if ((mm === "February") && (dd <= 0 || dd > 29) && (0 == yy % 4)) { 
+		document.getElementById("error").innerText = "Oops didn't selected a valid date!!" 
+	
+	} else if ((mm === "February") && (dd <= 0 || dd > 28) && (0 != yy % 4)) {  
+		document.getElementById("error").innerText = "Oops didn't selected a valid date!!" 
+		
+	} else if (male === true) { 
+		document.getElementById("result").innerHTML = "You were born on  " + days[dayOfTheWeek] + ".\n" + "Your Akan name is " + maleNames[dayOfTheWeek] + "!" //shows result by replacing the  HTML content in the id=result
+	
+	} else if (female === true) { 
+		document.getElementById("result").innerHTML = "You were born on  " + days[dayOfTheWeek] + ".\n" + "Your Akan name is " + femaleNames[dayOfTheWeek] + "!" //shows result by replacing the HTML content in the id=result
 
-
-
-
-
-
+	} else if ((female === false) && (male === false)) { 
+		document.getElementById("error").innerText = "Please select gender!" 
+		
+	}
 };
-
-
-
-
-
-
-
-
-
-
-// Deleting result function
-
-function deleteResult(e){
-    e.preventDefault();
-
-    document.getElementById("result");
-
- 
-};
-
-
-
-
