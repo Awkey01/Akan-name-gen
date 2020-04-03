b9f9ec3abecbbcbd46cbb1005dbfb8c876395fcd
Abdihakim Hassan <h_abdihakimi@yahoo.com>
2020-04-03 11:36:39 +0300

    save changes

--- js/main.js
+++ js/main.js
@@ -11,20 +11,20 @@ function showResult(e) {
   var day_of_the_week = birth_date.getDay();
 
   if (dd <= 0 || dd > 31) {
-    document.getElementById("error").innerText = "Please enter a valid date!"
+    alert("Please enter a valid date!");
   } else if ((mm === "September" || mm === "April" || mm === "June" || mm === "November") && (dd <= 0 || dd > 30)) {
-    document.getElementById("error").innerText = "Please enter a valid date!"
+    alert("Please enter a valid date!");
   } else if (mm === "------") {
-    document.getElementById("error").innerText = "Please select a month!"
+    alert("Please select a month!");
 
   } else if (yy <= 0000 || yy > 2020) {
-    document.getElementById("error").innerText = "Please enter a valid year!"
+    alert("Please enter a valid year!");
 
   } else if ((mm === "February") && (dd <= 0 || dd > 29) && (0 == yy % 4)) {
-    document.getElementById("error").innerText = "Please enter a valid date!"
+    alert("Please enter a valid date!");
 
   } else if ((mm === "February") && (dd <= 0 || dd > 28) && (0 != yy % 4)) {
-    document.getElementById("error").innerText = "Please enter a valid date!"
+    alert("Please enter a valid date!");
 
   } else if (male === true) {
     document.getElementById("result").innerHTML = "You were born on a " + days[day_of_the_week] + ".\n" + "Your Akan name is " + male_names[day_of_the_week] + "!"
@@ -32,14 +32,16 @@ function showResult(e) {
   } else if (female === true) {
     document.getElementById("result").innerHTML = "You were born on a " + days[day_of_the_week] + ".\n" + "Your Akan name is " + female_names[day_of_the_week] + "!"
   } else if ((female === false) && (male === false)) {
-    document.getElementById("error").innerText = "Please select gender!"
+    alert("Please select gender!");
 
+  } else {
+    alert("Please fill the form");
   }
 }
 
 
 function Cancel(e) {
   document.getElementById("result");
-  document.getElementById("error");
+  
 
 }
